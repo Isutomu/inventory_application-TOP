18c7c9061551109c190aaf05e555407af500cd43
Isutomu <isutomu@gmail.com>
2024-11-04 13:47:44 -0300

    :sparkles: feat: enable create/delete routes for songs

--- controller.js
+++ controller.js
@@ -47,6 +47,37 @@ const updateSongPost = asyncHandler(async (req, res) => {
   });
   res.redirect("/");
 });
+const addSongGet = asyncHandler(async (req, res) => {
+  const singers = await queries.getAllSingers();
+  const albums = await queries.getAllAlbums();
+  res.render("add_song", { singers, albums });
+});
+const addSongPost = asyncHandler(async (req, res) => {
+  const singer =
+    typeof req.body.singer == "string" ? [req.body.singer] : req.body.singer;
+  const singerIdPromises = singer.map(async (singerName) => {
+    const singerInfo = await queries.getSingerByName(singerName);
+    return singerInfo["id"];
+  });
+  const singerId = await Promise.all(singerIdPromises);
+
+  const album = req.body.album;
+  const albumInfo = await queries.getAlbumByName(album);
+  const albumId = albumInfo["id"];
+
+  await queries.addSong({
+    title: req.body.title,
+    singerId,
+    albumId,
+    link: req.body.link,
+  });
+  res.redirect("/");
+});
+const deleteSongPost = asyncHandler(async (req, res) => {
+  const songId = req.params.id;
+  await queries.deleteSong(songId);
+  res.redirect("/songs");
+});
 
 module.exports = {
   renderHomepage,
@@ -55,4 +86,7 @@ module.exports = {
   getAllAlbums,
   updateSongGet,
   updateSongPost,
+  addSongGet,
+  addSongPost,
+  deleteSongPost,
 };
--- routes.js
+++ routes.js
@@ -9,5 +9,8 @@ dbRouter.get("/singers", controller.getAllSingers);
 dbRouter.get("/albums", controller.getAllAlbums);
 dbRouter.get("/songs/:id", controller.updateSongGet);
 dbRouter.post("/songs/:id", controller.updateSongPost);
+dbRouter.get("/new/songs", controller.addSongGet);
+dbRouter.post("/new/songs", controller.addSongPost);
+dbRouter.post("/delete/songs/:id", controller.deleteSongPost);
 
 module.exports = dbRouter;
